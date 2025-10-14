2d48c99ef614160eea24455c22ee2cd31cf99a0f
sedg <hyde.rou@yandex.ru>
2025-10-14 14:22:09 +0300

    explore Body and Logo components

--- app/wine/whinepad/src/components/Discovery.js
+++ app/wine/whinepad/src/components/Discovery.js
@@ -1,18 +1,23 @@
 import Excel from './Excel';
+import Logo from './Logo';
+import Header from './Header';
+import Body from './Body';
 
 function Discovery() {
     return (
-        <div>
-            <h2>Excel</h2>
-            <Excel
-                headers={['Name', 'Year']}
-                initialData={[
-                    ['Charles', '1859'],
-                    ['Antoine', '1943'],
-                ]}
-            />
-            {/* сюда помещаются дополнительные компоненты */}
+        <div className="Discovery">
+            <h2>Logo</h2>
+            <div style={{
+                background: '#f6f6f6',
+                display: 'inline-block'
+            }}>
+                <Logo />
+            </div>
+            <h2>Body</h2>
+            <Body>I am content inside the body</Body>
+            {/* и т. д. */}
         </div>
     );
 }
+
 export default Discovery;
