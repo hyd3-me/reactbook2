52d5f620a8ef17d355df0c1aa17fc31644d471c5
sedg <hyde.rou@yandex.ru>
2025-10-14 13:33:31 +0300

    init react app: whinepad

---
+++ app/wine/whinepad/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Excel from './components/Excel';
+
+function App() {
+  return (
+    <div>
+      <Excel
+        headers={['Name', 'Year']}
+        initialData={[
+          ['Charles', '1859'],
+          ['Antoine', '1943'],
+        ]}
+      />
+    </div>
+  );
+}
+
+export default App;
